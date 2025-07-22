763ab58ccca3f7999079f659384c414cbdbb8dae
Arpana Sharma <arsharma09@protonmail.com>
2025-07-22 14:56:59 +0530

    message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx or src/app/layout.tsx
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
